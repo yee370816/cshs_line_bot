bdc4559d7ba72ad14f2e0cf15522dfef4640b778
Eric Lin <eric.yijuin.lin@gmail.com>
2023-10-19 17:09:03 +0800

    update student module loading

--- utilities.py
+++ utilities.py
@@ -1,17 +1,19 @@
 import json
 import importlib
+import time
+import os
 
-def read_config(path: str) -> dict:
+def read_config(file_path: str) -> dict:
     try:
-        with open(path, 'r', encoding='utf-8') as fs:
+        with open(file_path, 'r', encoding='utf-8') as fs:
             return json.load(fs) # channelSecret & accessToken
     except FileNotFoundError:
         print('failed to load credential')
         quit("the LINE bot API is terminated")
 
-def read_students(path: str) -> dict:
+def read_students(file_path: str) -> dict:
     try:
-        with open(path, 'r', encoding='utf-8') as fs:
+        with open(file_path, 'r', encoding='utf-8') as fs:
             return json.load(fs) # channelSecret & accessToken
     except FileNotFoundError:
         print('failed to load student data')
@@ -21,6 +23,15 @@ def get_student_modules(student_data: dict, parent_module = "student_modules") -
     student_modules = {}
     for line_id, student_data in student_data.items():
         module_path = parent_module  + "." + student_data["student_no"]
-        student_module = importlib.import_module(module_path)
-        student_modules[line_id] = student_module
+        if importlib.util.find_spec(module_path):
+            student_module = importlib.import_module(module_path)
+            student_modules[line_id] = student_module
+        else:
+            debug = True
+            module_path = "./" + parent_module  + "/" + student_data["student_no"] + ".py"
+            with open(module_path, 'w', encoding='utf-8') as fs:
+                fs.write("def process(message: str) -> None:\n")
+                fs.write("    pass\n")
+                fs.close()
+
     return student_modules
