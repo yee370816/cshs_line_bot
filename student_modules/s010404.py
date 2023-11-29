951728859154e6e43db0f3ec08b6ee20520d6ffb
cshs010404 <150873062+cshs010404@users.noreply.github.com>
2023-11-29 09:52:05 +0800

    Update s010404.py

--- student_modules/s010404.py
+++ student_modules/s010404.py
@@ -1,2 +1,9 @@
 def process(message: str) -> None:
-    pass
+    if message=="蔡雨臻":
+        return "幹嘛"
+    if message=="你好":
+        return "有事?"
+    if message=="有":
+        return "有事說，但我不會幫你處理"
+    if message=="沒事":
+        return "...6"
