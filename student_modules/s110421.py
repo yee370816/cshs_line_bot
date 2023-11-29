dbdb7fadd1cad524b3f2db8306ab05bc7234595d
110421student <148313301+110421student@users.noreply.github.com>
2023-11-29 13:58:48 +0800

    Update s110421.py

--- student_modules/s110421.py
+++ student_modules/s110421.py
@@ -1,2 +1,11 @@
 def process(message: str) -> None:
-    pass
+    if message == "debug":
+        return message
+    if message == "資訊社":
+        return "讚"
+    if message == "資訊老師":
+        return "帥"
+    if message == "資訊社的同學":
+        return "棒"
+    if message == "資訊社的電腦":
+        return "不好說"
