4bd7682f9d3bb512a76ba4cfd3bfebc66d0d1046
GINO01112 <148313489+GINO01112@users.noreply.github.com>
2023-11-29 14:13:43 +0800

    Update s210714.py

--- student_modules/s210714.py
+++ student_modules/s210714.py
@@ -1,2 +1,12 @@
 def process(message: str) -> None:
-    pass
+    passdef process(message: str) -> None:
+    if message == "debug":
+        return message
+    if message == "資訊社":
+        return "讚"
+    if message == "資訊老師":
+        return "帥"
+    if message == "資訊社的同學":
+        return "棒"
+    if message == "資訊社的電腦":
+        return "不好說"
