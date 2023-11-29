5d045383697ab9dd0511aa3b9748d9556797ed8d
cshs010507 <150873055+cshs010507@users.noreply.github.com>
2023-11-29 09:57:10 +0800

    Update s010507.py

--- student_modules/s010507.py
+++ student_modules/s010507.py
@@ -1,2 +1,7 @@
 def process(message: str) -> None:
-    pass
+    if message == "哈囉啊!":
+        return "哈囉啊!"
+    if message == "在嗎":
+        return "不在"
+    if message == "早上好":
+        return "早"
