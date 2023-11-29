9f64eb4571893c759533eefd9d07adbd5fa554ba
cshs010112 <150873053+cshs010112@users.noreply.github.com>
2023-11-29 09:51:33 +0800

    Update s010112.py

--- student_modules/s010112.py
+++ student_modules/s010112.py
@@ -1,2 +1,9 @@
 def process(message: str) -> None:
-    pass
+    if message=="李佳霈":
+      return "SB"
+    if message=="你好":
+      return "好個屁"
+    if message=="嘻嘻嘻":
+      return "哈哈哈"
+    if message=="雞你太美":
+      return "實在是太美"
